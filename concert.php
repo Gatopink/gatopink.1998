bd7a58039fca4e3bd9c060e49ca4b4cf00f62bf8
Gatopink <julietuit@outlook.com>
2024-04-28 22:08:02 -0400

    concertphp

---
+++ concert.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Concert Form </title>
+  <link href="concert.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+  <div class="container">
+    <header class = "fctr">
+	<h1 class="white">Summary of Concert Ticket Cost</h1>
+    </header>
+    
+    
+    
+    <footer class = "fctr">
+        <a class="white" href="concert_form.html">Return to Form Entry To Continue</a>
+    </footer>
+  </div> 
+</body>
+</html>
